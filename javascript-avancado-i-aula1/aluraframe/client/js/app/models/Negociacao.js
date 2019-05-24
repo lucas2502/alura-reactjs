c1be054db9bf9e2939da2df03b4f9d7f903b900f
lucas2502 <lucas2502@live.com>
2019-05-24 16:56:56 -0300

    AULA-04 - Criando ListModel

--- javascript-avancado-i-aula1/aluraframe/client/js/app/models/Negociacao.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/models/Negociacao.js
@@ -1,8 +1,8 @@
 class Negociacao {
 
-        constructor(date, quantiade, valor) {
+        constructor(data, quantiade, valor) {
 
-            this._date = date;
+            this._data = new Date(data.getTime());
             this._quantidade = quantiade;
             this._valor = valor;
             Object.freeze(this);
@@ -13,7 +13,7 @@ class Negociacao {
         }
 
         get data() {
-            return this._data;
+            return new Date(this._data.getTime());
         }
 
         get quantiade() {
@@ -24,5 +24,37 @@ class Negociacao {
             return this._valor;
         }
 
+        get volume() {
+           let volume = 0;
+            return volume = this._quantidade * this._valor;
+        }
 
+
+}
+
+class Conta {
+
+    constructor(titular, conta) {
+
+        this._titular = titular;
+        this._conta = conta;
+        this._saldo = 0.0;
+    }
+
+    deposita(valor) {
+        console.log('Valor depositado: ' + valor);
+        this._saldo+=valor; 
+    }
+
+    get saldo(){
+        return this._saldo
+    }
+
+    get titular() {
+        return this._titular;
+    }
+
+    get conta() {
+        return this._conta;
+    }
 }
