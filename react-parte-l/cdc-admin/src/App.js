a4bb51e814de774f66c2521719e0dd216cac0a8c
lucas2502 <lucas2502@live.com>
2019-07-01 11:57:16 -0300

    AULA-04 Manutenção de estado form

--- react-parte-l/cdc-admin/src/App.js
+++ react-parte-l/cdc-admin/src/App.js
@@ -7,22 +7,65 @@ import $ from 'jquery'
 class App extends Component {
   constructor() {
     super();
-    this.state = {lista: []}
+    this.state = {
+      lista: [], 
+      nome: '', 
+      email:'', 
+      senha: ''
+    }
+    this.enviaForm = this.enviaForm.bind(this)
+    this.setNome = this.setNome.bind(this)
+    this.setEmail = this.setEmail.bind(this)
+    this.setSenha = this.setSenha.bind(this)
   }
 
-  componentWillMount(){
-    console.log("WillMount")
+  componentDidMount(){
     $.ajax({
       url: "https://cdc-react.herokuapp.com/api/autores",
       dataType: 'json',
       success: function(resposta) {
-        console.log("Chegou a resposta!")
         this.setState({ lista: resposta });
       }.bind(this)
     })
   }
+
+  enviaForm(e){
+    e.preventDefault();
+    $.ajax({
+      url: 'http://localhost:8080/api/autores',
+      contentType:'aplication/json',
+      dataType: 'json',
+      type: 'post',
+      data: JSON.stringify({
+        nome: this.state.nome, 
+        email: this.state.email, 
+        senha: this.state.senha
+      }),
+      sucess: function(res){
+        console.log('enviado com sucesso')
+      },
+      error: function(res){
+        console.log('error')
+      }
+    })
+  }
+
+  setNome(e){
+    
+    this.setState({nome: e.target.value})
+  }
+
+  setEmail(e){
+    
+    this.setState({email: e.target.value})
+  }
+
+  setSenha(e){
+    
+    this.setState({senha: e.target.value})
+  }
+
   render() {
-    console.log("render")
     return (
           <div id="layout">
             <a href="#menu" id="menuLink" className="menu-link">    
@@ -50,18 +93,18 @@ class App extends Component {
               </div>
               <div className="content" id="content">
                 <div className="pure-form pure-form-aligned">
-                  <form className="pure-form pure-form-aligned">
+                  <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm.bind(this)} method="post">
                     <div className="pure-control-group">
                       <label htmlFor="nome">Nome</label> 
-                      <input id="nome" type="text" name="nome" value=""  />                  
+                      <input id="nome" type="text" name="nome" value={this.state.nome}  onChange={this.setNome}/>                  
                     </div>
                     <div className="pure-control-group">
                       <label htmlFor="email">Email</label> 
-                      <input id="email" type="email" name="email" value=""  />                  
+                      <input id="email" type="email" name="email" value={this.state.email}  onChange={this.setEmail} />                  
                     </div>
                     <div className="pure-control-group">
                       <label htmlFor="senha">Senha</label> 
-                      <input id="senha" type="password" name="senha"  />                                      
+                      <input id="senha" type="password" name="senha" value={this.state.senha}   onChange={this.setSenha}/>                                      
                     </div>
                     <div className="pure-control-group">                                  
                       <label></label> 
@@ -82,7 +125,7 @@ class App extends Component {
                       {
                         this.state.lista.map(autor => {
                           return (
-                            <tr>
+                            <tr key={autor.id}>
                               <td>{autor.nome}</td>
                               <td>{autor.email}</td>
                             </tr>
