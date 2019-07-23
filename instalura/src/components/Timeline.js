cff5ee458bd2257c7ed8ec01ecb0dece35f5c82f
Lucas Gonçalves <lucas2502@live.com>
2019-07-23 15:40:52 -0300

    Aula-01

---
+++ instalura/src/components/Timeline.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Foto from './Foto'
+
+const Timeline = () => {
+    return(
+        <div className="fotos container">
+          <Foto />
+
+        </div>
+    )
+}
+
+export default Timeline
